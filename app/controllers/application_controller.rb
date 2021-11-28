5c0a0a8e72f29fc0ff78acf71b80db0393bf8391
Tepen <elcabron.penyah@gmail.com>
2021-11-29 02:11:41 +0800

    permit firstname and lastname parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
     return "/books"
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:first_name, :last_name, :email]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
 end
