f0a969167f041257d77842dabdb2a5d7e8cf9ed7
Tepen <elcabron.penyah@gmail.com>
2021-11-29 17:26:53 +0800

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create([
+  { email: "admin@lib.com", password: "admin123", first_name: "Admin", last_name: "User", admin: true },
+  { email: "student@lib.com", password: "student123", first_name: "Student", last_name: "User", admin: false }
+])
+
+students = Student.create([
+  { lrn: "888888", firstname: "Tony", lastname: "Stark", contact_number: "0912351234" },
+  { lrn: "121221", firstname: "Steve", lastname: "Rogers", contact_number: "0912351234" }
+])
+
+puts "Users seeded!"
